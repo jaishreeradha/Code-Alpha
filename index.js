bcc4c85f313813bc3c5da0d0f6a724b2dce3ec23
Devraj Soni <132013282+jaishreeradha@users.noreply.github.com>
2024-10-31 23:28:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-function Solve(val) {
-    var v = document.getElementById('res');
-    v.value += val;
+function openModal(imageSrc) {
+    let modal = document.getElementById("myModal");
+    let modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
 }
-function Result() {
-    var num1 = document.getElementById('res').value;
-    try {
-        var num2 = eval(num1.replace('x', '*'));
-        document.getElementById('res').value = num2;
-    } catch {
-        document.getElementById('res').value = 'Error';
-    }
+
+function closeModal() {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "none";
 }
-function Clear() {
-    var inp = document.getElementById('res');
-    inp.value = '';
-}
-function Back() {
-    var ev = document.getElementById('res');
-    ev.value = ev.value.slice(0, -1);
-}
-document.addEventListener('keydown', function (event) {
-    const key = event.key;
-    const validKeys = '0123456789+-*/.%';
-    if (validKeys.includes(key)) {
-        Solve(key === '*' ? 'x' : key);
-    } else if (key === 'Enter') {
-        Result();
-    } else if (key === 'Backspace') {
-        Back();
-    } else if (key.toLowerCase() === 'c') {
-        Clear();
-    }
-});
